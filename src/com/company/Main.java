cd4f2cb15e6f694475d38d1ef2ec78b8bbc14743
R. Lama <r.lama@avanade.com>
2020-05-24 23:39:07 -0500

    If else commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,8 +3,14 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code her
-            System.out.println("Hello World");
-        System.out.println("Whats up");
-    }
-}
+	// assign value
+          int i =6;
+          // check the value
+        if (i==5) {
+            System.out.println("the value of integer is 5" );}
+        else if (i==6){
+            System.out.println("the value of integer is 6");}
+        else {
+            System.out.println("value of integer is not 5 and 6");
+        }
+}}
